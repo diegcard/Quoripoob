fb527dfe0b70c7448650b2f10e0610fb724d8af9
Diego Cardenas <diegosp778@gmail.com>
2024-04-29 12:56:07 -0500

    Interfza aprinicpal OK

--- Quoripoob/src/presentation/QuoripoobGUI.java
+++ Quoripoob/src/presentation/QuoripoobGUI.java
@@ -1,17 +1,199 @@
 package Quoripoob.src.presentation;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
 public class QuoripoobGUI extends JFrame {
 
-    public QuoripoobGUI() {
-        super("Quoripoob");
-        setSize(1024, 720);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+    // Constants Screen
+    private static final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private static final int WIDTH = (int) (3 *screenSize.getWidth()/4);
+    private static final int HEIGHT = (int) (3 *screenSize.getHeight()/4);
+
+    // Panel Menu
+    private JMenuItem itemNewGame;
+    private JMenuItem itemLoadGame;
+    private JMenuItem itemSaveGame;
+    private JMenuItem itemExit;
+
+    // Panel Game
+    private JPanel panelGame;
+
+    /**
+     * Method to prepare the menu
+     * @return JMenuBar
+     */
+    private JMenuBar preparateElementsMenu(){
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menuGame = new JMenu("File");
+        itemNewGame = new JMenuItem("New Game");
+        itemLoadGame = new JMenuItem("Load Game");
+        itemSaveGame = new JMenuItem("Save Game");
+        itemExit = new JMenuItem("Exit");
+        menuGame.add(itemNewGame);
+        menuGame.add(itemLoadGame);
+        menuGame.add(itemSaveGame);
+        menuGame.addSeparator();
+        menuGame.add(itemExit);
+        menuBar.add(menuGame);
+        return menuBar;
     }
 
+    /**
+     * Method to prepare the elements of the screen
+     */
+    private void preparateElements() {
+        try {
+            UIManager.setLookAndFeel(new NimbusLookAndFeel());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        setTitle("Quoripoob");
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        setJMenuBar(preparateElementsMenu());
+        prepareElemetsStart();
+    }
+
+    /**
+     * Method to prepare the actions of the menu
+     */
+    private void prepareMenuActions() {
+        itemExit.addActionListener(e -> closeOption());
+    }
+
+    /**
+     * Method to prepare the elements of the start screen
+     */
+    private void prepareElemetsStart() {
+        JPanel panelStart = new JPanel(new GridLayout(6, 5, 10, 10)); // Establecer el GridLayout con espaciado
+        panelStart.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20)); // Establecer un borde interno
+        // Agregar etiquetas vacías en la primera fila
+        for (int i = 0; i < 7; i++) {
+            panelStart.add(new JLabel());
+        }
+        // Agregar el título
+        JLabel labelTitle = new JLabel("Quoripoob");
+        labelTitle.setFont(new Font("Arial", Font.BOLD, 30));
+        labelTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        labelTitle.setText("<html><font color='black'>Quoripoob</font></html>");
+        panelStart.add(labelTitle);
+        // Agregar etiquetas vacías en la segunda fila
+        for (int i = 0; i < 4; i++) {
+            panelStart.add(new JLabel());
+        }
+        // Agregar la imagen
+        ImageIcon image = new ImageIcon("Quoripoob/src/presentation/Img/Fondo.jpeg");
+        JLabel labelImage = new JLabel(image) {
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+                g2d.drawImage(image.getImage(), 0, 0, getWidth(), getHeight(), null);
+            }
+        };
+        panelStart.add(labelImage);
+        // Agregar etiquetas vacías en la cuarta fila
+        for (int i = 0; i < 4; i++) {
+            panelStart.add(new JLabel());
+        }
+        // Agregar el botón "Play"
+        JButton buttonPlay = new JButton("Play");
+        buttonPlay.setFont(new Font("Arial", Font.BOLD, 25));
+        buttonPlay.setForeground(Color.WHITE);
+        buttonPlay.setBackground(new Color(51, 153, 255));
+        buttonPlay.setBorder(BorderFactory.createRaisedBevelBorder());
+        buttonPlay.setFocusPainted(false);
+        buttonPlay.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                buttonPlay.setBackground(new Color(102, 178, 255));
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                buttonPlay.setBackground(new Color(51, 153, 255));
+            }
+        });
+        panelStart.add(buttonPlay);
+        // Agregar etiquetas vacías en la última fila
+        for (int i = 0; i < 4; i++) {
+            panelStart.add(new JLabel());
+        }
+        //Boton Creditos
+        JButton buttonCredits = new JButton("Credits");
+        buttonCredits.setFont(new Font("Arial", Font.BOLD, 25));
+        buttonCredits.setForeground(Color.WHITE);
+        buttonCredits.setBackground(new Color(51, 153, 255));
+        buttonCredits.setBorder(BorderFactory.createRaisedBevelBorder());
+        buttonCredits.setFocusPainted(false);
+        buttonCredits.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                buttonCredits.setBackground(new Color(102, 178, 255));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                buttonCredits.setBackground(new Color(51, 153, 255));
+            }
+        });
+        panelStart.add(buttonCredits);
+        for (int i = 0; i < 2; i++) {
+            panelStart.add(new JLabel());
+        }
+        add(panelStart, BorderLayout.CENTER);
+    }
+
+    /**
+     * Method to prepare the actions
+     */
+    private void preparateActions() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent event) {
+                closeOption();
+            }
+        });
+        prepareMenuActions();
+    }
+
+    /**
+     * Constructor
+     */
+    private QuoripoobGUI() {
+        preparateElements();
+        preparateActions();
+
+    }
+
+    /**
+     * Method to close the application
+     */
+    private void closeOption(){
+        int yesNo = JOptionPane.showOptionDialog(null, "Are you sure you want exit?", "Warning",
+                JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE, null, null, "No");
+        if (yesNo == JOptionPane.YES_OPTION) {
+            setVisible(false);
+            System.exit(0);
+        }
+    }
+
+    /**
+     * Main method
+     * @param args Arguments
+
+     */
     public static void main(String[] args) {
-        new QuoripoobGUI();
+        QuoripoobGUI gui = new QuoripoobGUI();
+        gui.setVisible(true);
     }
 }
